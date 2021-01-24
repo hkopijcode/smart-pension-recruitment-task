eb681a5cd774f17e1a8a44c1415b34ed0f6279ef
hkopijcode <hubertkopijcode@gmail.com>
2021-01-24 20:02:08 +0100

    change: specs sample file, add happy path

--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -1,18 +1,34 @@
 require_relative '../../lib/log_parser'
 
 describe LogParser do
-  subject { described_class.new(log_path).process }
+  subject { described_class.new(log_path) }
 
   context 'when valid file is provided' do
     let(:log_path) { 'spec/fixtures/sample.log'}
-    # TODO
+
+    it 'returns valid hash' do
+      subject.process
+      expect(subject.views).to eq({
+        "/home"=>["682.704.613.213",
+          "682.704.613.213",
+          "682.704.613.213",
+          "682.704.613.213",
+          "682.704.613.213",
+          "682.704.613.213",
+          "682.704.613.213",],
+        "/about/2"=>["715.156.286.412",
+          "715.156.286.412",
+          "715.156.286.412"]
+        })
+      expect(subject.views.count).to eq(2)
+    end
   end
 
   context 'when invalid file is provided' do
     let(:log_path) { 'spec/fixtures/invalid_sample.rb'}
 
     it 'raises valid error' do
-      expect { subject }.to raise_error(StandardError, 'Provided file is not valid log file')
+      expect { subject.process }.to raise_error(StandardError, 'Provided file is not valid log file')
     end
   end
 
@@ -20,7 +36,7 @@ describe LogParser do
     let(:log_path) {  }
 
     it 'raises valid error' do
-      expect { subject }.to raise_error(StandardError, 'Filename can not be blank')
+      expect { subject.process }.to raise_error(StandardError, 'Filename can not be blank')
     end
   end
 end
