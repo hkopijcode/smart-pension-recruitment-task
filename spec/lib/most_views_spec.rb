b220a734c055c40a9dd15438c89c4c0a1bcda744
hkopijcode <hubertkopijcode@gmail.com>
2021-01-24 21:47:47 +0100

    change: specs to validate if returned hashes are sorted

--- spec/lib/most_views_spec.rb
+++ spec/lib/most_views_spec.rb
@@ -1,20 +1,6 @@
 require_relative '../../lib/most_views'
 
 describe MostViews do
-  let(:views_hash) do
-    {
-      '/home'=>['682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',],
-      '/about/2'=>['715.156.286.412',
-                   '715.156.286.412',
-                   '715.156.286.412']
-      }
-  end
   let(:most_views) do
     {
       '/home'=>7,
@@ -23,7 +9,45 @@ describe MostViews do
   end
   subject { described_class.new(views_hash).call }
 
-  it 'returns correct hash' do
-    expect(subject).to eq(most_views)
+  context 'when views_hash is sorted correctly' do
+    let(:views_hash) do
+      {
+        '/home'=>['682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',],
+        '/about/2'=>['715.156.286.412',
+                     '715.156.286.412',
+                     '715.156.286.412']
+        }
+    end
+
+    it 'returns correct hash' do
+      expect(subject).to eq(most_views)
+    end
+  end
+
+  context 'when views_hash is not sorted' do
+    let(:views_hash) do
+      {
+        '/about/2'=>['715.156.286.412',
+                     '715.156.286.412',
+                     '715.156.286.412'],
+        '/home'=>['682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',]
+        }
+    end
+
+    it 'returns correct hash' do
+      expect(subject).to eq(most_views)
+    end
   end
 end
--- spec/lib/unique_views_spec.rb
+++ spec/lib/unique_views_spec.rb
@@ -1,29 +1,53 @@
 require_relative '../../lib/unique_views'
 
 describe UniqueViews do
-  let(:views_hash) do
-    {
-      '/home'=>['682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',
-                '682.704.613.213',],
-      '/about/2'=>['715.156.286.412',
-                   '715.156.286.412',
-                   '715.156.286.412']
-      }
-  end
   let(:unique_views) do
     {
-      '/home'=>1,
+      '/home'=>2,
       '/about/2'=>1
     }
   end
   subject { described_class.new(views_hash).call }
 
-  it 'returns correct hash' do
-    expect(subject).to eq(unique_views)
+  context 'when views_hash is sorted correctly' do
+    let(:views_hash) do
+      {
+        '/home'=>['682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '715.156.286.412'],
+        '/about/2'=>['715.156.286.412',
+                     '715.156.286.412',
+                     '715.156.286.412']
+        }
+    end
+
+    it 'returns correct hash' do
+      expect(subject.to_a).to eq(unique_views.to_a)
+    end
+  end
+
+  context 'when views_hash is not sorted' do
+    let(:views_hash) do
+      {
+        '/about/2'=>['715.156.286.412',
+                     '715.156.286.412',
+                     '715.156.286.412'],
+        '/home'=>['682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '682.704.613.213',
+                  '715.156.286.412']
+        }
+    end
+
+    it 'returns correct hash' do
+      expect(subject.to_a).to eq(unique_views.to_a)
+    end
   end
 end
