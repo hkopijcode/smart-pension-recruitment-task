02da9ba295adc7472fc17af05d0e1e345ef2f3a3
hkopijcode <hubertkopijcode@gmail.com>
2021-01-24 21:53:56 +0100

    add: unique_views

---
+++ lib/unique_views.rb
@@ -0,0 +1,25 @@
+class UniqueViews
+  attr_reader :views_hash, :unique_views
+
+  def call
+    generate_unique_views
+    sort_unique_views
+  end
+
+  private
+
+  def initialize(views_hash)
+    @views_hash = views_hash
+    @unique_views = {}
+  end
+
+  def generate_unique_views
+    views_hash.each do |k, _v|
+      unique_views[k] = views_hash[k].uniq.count
+    end
+  end
+
+  def sort_unique_views
+    unique_views.sort_by { |webpage, count| -count }
+  end
+end
